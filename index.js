8f25d76e03d54885d421dc8343e24294c585d4ed
kek <oleh.petryk@gmail.com>
2017-12-12 00:33:06 +0200

    c

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ for(var i = 0; i < keys.length; i++) {
 		var inputVal = input.innerHTML;
 		var btnVal = this.innerHTML;
 		if(btnVal === 'C') {
+			input.innerHTML = '';
+			decimalAdded = false;
 		}
 		else if(btnVal === '=') {
 			var equation = inputVal;
