3311ac7c89792eb9a635009bcfbc36216d6e2c80
kek <oleh.petryk@gmail.com>
2017-12-12 00:37:39 +0200

    c

--- index.js
+++ index.js
@@ -14,15 +14,20 @@ for (var i = 0; i < keys.length; i++) {
       var equation = inputVal;
       var lastChar = equation[equation.length - 1];
       equation = equation.replace(/x/g, "*").replace(/÷/g, "/");
-      if (operators.indexOf(lastChar) > -1 || lastChar === ".")
+      if (operators.indexOf(lastChar) > -1 || lastChar === ".") {
         equation = equation.replace(/.$/, "");
-      if (equation) input.innerHTML = eval(equation);
+      }
+      if (equation) {
+        input.innerHTML = eval(equation);
+      }
       decimalAdded = false;
     } else if (operators.indexOf(btnVal) > -1) {
       var lastChar = inputVal[inputVal.length - 1];
-      if (inputVal != "" && operators.indexOf(lastChar) === -1)
+      if (inputVal != "" && operators.indexOf(lastChar) === -1) {
         input.innerHTML += btnVal;
-      else if (inputVal === "" && btnVal === "-") input.innerHTML += btnVal;
+      } else if (inputVal === "" && btnVal === "-") {
+        input.innerHTML += btnVal;
+      }
       if (operators.indexOf(lastChar) > -1 && inputVal.length > 1) {
         input.innerHTML = inputVal.replace(/.$/, btnVal);
       }
