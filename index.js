a80bb35a9701f128b08b3b11bd8d407ab4d358c6
kek <oleh.petryk@gmail.com>
2017-12-12 01:19:04 +0200

    c

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var operators = ["+", "-", "x", "÷"];
 var decimalAdded = false;
 
 for (var i = 0; i < keys.length; i++) {
-	
   keys[i].onclick = function(e) {
+	debugger;
     var input = document.querySelector(".screen");
     var inputVal = input.innerHTML;
 	var btnVal = this.innerHTML;
