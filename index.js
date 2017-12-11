7fc5e0b46d4e27f12e46526c6aeca705d31551ef
kek <oleh.petryk@gmail.com>
2017-12-12 01:52:01 +0200

    c

--- index.js
+++ index.js
@@ -4,16 +4,15 @@ var decimalAdded = false;
 
 for (var i = 0; i < keys.length; i++) {
   keys[i].onclick = function(e) {
-
     var input = document.querySelector(".screen");
     var inputVal = input.innerHTML;
-	var btnVal = this.innerHTML;
+    var btnVal = this.innerHTML;
     if (btnVal === "C") {
       input.innerHTML = "";
       decimalAdded = false;
     } else if (btnVal === "=") {
       var equation = inputVal;
-	  var lastChar = equation[equation.length - 1];
+      var lastChar = equation[equation.length - 1];
       equation = equation.replace(/x/g, "*").replace(/÷/g, "/");
       if (operators.indexOf(lastChar) > -1 || lastChar === ".") {
         equation = equation.replace(/.$/, "");
@@ -23,7 +22,13 @@ for (var i = 0; i < keys.length; i++) {
       }
       decimalAdded = false;
     } else if (operators.indexOf(btnVal) > -1) {
+     
       var lastChar = inputVal[inputVal.length - 1];
+      if (input.innerHTML === "Infinity"&&btnVal=== "-") {
+        input.innerHTML = "";
+        input.innerHTML += btnVal;
+      }
+      else
       if (inputVal != "" && operators.indexOf(lastChar) === -1) {
         input.innerHTML += btnVal;
       } else if (inputVal === "" && btnVal === "-") {
@@ -34,17 +39,18 @@ for (var i = 0; i < keys.length; i++) {
       }
       decimalAdded = false;
     } else if (btnVal === ".") {
+      if (input.innerHTML === "Infinity") {
+        input.innerHTML = "";
+      }
       if (!decimalAdded) {
         input.innerHTML += btnVal;
         decimalAdded = true;
       }
     } else {
+      if (input.innerHTML === "Infinity") {
+        input.innerHTML = "";
+      }
       input.innerHTML += btnVal;
-    }
-    if (input.innerHTML === "Infinity") {
-      alert("Bad action alert!");
-      input.innerHTML = "";
-      decimalAdded = false;
     }
     e.preventDefault();
   };
