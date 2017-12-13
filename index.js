5ea255f090872cc08f0e79a748d3bdb5b8fc5534
kek <oleh.petryk@gmail.com>
2017-12-13 03:32:07 +0200

    c

--- index.js
+++ index.js
@@ -69,7 +69,10 @@ for (var i = 0; i < keys.length; i++) {
         }
       } else if (btnVal === "(") {
         var lastChar = input.innerHTML[inputVal.length - 1];
-        if (operators.lastIndexOf(lastChar) === -1) {
+        if (input.innerHTML === "") {
+          input.innerHTML += btnVal;
+          openCount++;
+        } else if (isNaN(lastChar)===false) {
           input.innerHTML += "";
         } else if (lastChar === ")") {
           input.innerHTML += "";
